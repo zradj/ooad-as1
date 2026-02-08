437ae0b32093ef065f4c5e04dee33802cfd96f45
Zaur Rajabov <zradj7@gmail.com>
2026-02-08 23:09:21 +0400

    implement statistics method

--- src/Generator.java
+++ src/Generator.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,6 +28,26 @@ public class Generator {
             default -> throw new IllegalArgumentException("No method with number " + randNumGen);
         };
         return stream.collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    ArrayList<Double> statistics(ArrayList<Double> randomValues) {
+        double n = randomValues.size();
+
+        if (n == 0) return new ArrayList<>(Arrays.asList(0.0, 0.0, 0.0, 0.0, 0.0));
+
+        double mean = 0.0, min = Double.POSITIVE_INFINITY, max = Double.NEGATIVE_INFINITY;
+        for (Double x : randomValues) {
+            mean += x;
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        mean /= n;
+
+        double stddev = 0.0;
+        for (Double x : randomValues) stddev += Math.pow(x - mean, 2);
+        if (n > 1) stddev = Math.sqrt(stddev / (n - 1));
+
+        return new ArrayList<>(Arrays.asList(n, mean, stddev, min, max));
     }
 
     public static void main(String[] args) {
